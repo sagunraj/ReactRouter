b891d425bd7f7e127e8455f18fc79d408c469847
sagunraj <sagsraj@yahoo.com>
2018-11-20 16:50:07 +0545

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Switch, Route, Link} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <ul>
+        <li>
+        <Link to = "/">Home</Link>
+        </li>
+        <li>
+        <Link to = "/about">About</Link>
+        </li>
+      </ul>
+        <Switch>
+            <Route exact path = "/" component = {()=><div>This is the home div.</div>}/>
+            <Route path = "/about" component = {()=><div>This is the about div.</div>}/>
+        </Switch>
       </div>
     );
   }
